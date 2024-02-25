c95f35832d9777982a7b0d9f3354829355ad73ad
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-26 00:22:25 +0300

    update all

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -7,10 +7,6 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 function Navbar(){
     return (
         <div className='Navbar'>
-            <div className='window'>
-                <div className='option'>Cancel</div>
-            </div>
-            <div className="dim"></div>
             <div className="navbar-title">My Notes</div>
             <div className="navbar-options">
                 <a href="#" className="navbar-option">New</a>
@@ -20,10 +16,36 @@ function Navbar(){
 }
 
 function Home() {
-    
+    const [ListContent, setListContent] = useState([]);
+    function handleCancel(){
+
+    }
+
+    function handleCreate(data){   
+        setListContent([notesContent,
+              { id: notesContent.length+1, title: noteTitle, description:noteDescription , createdAt: current_time},
+            ]);
+            document.getElementById("showFormbtn").style.display = "block";
+            document.getElementById("note-title").value = "" ; 
+            document.getElementById("note-body").value = "" ; 
+          
+          }
+
+    }
+    const handleInput = event => {
+        event.target.value
+      };
+
+
 
     return (
         <div className='Home'>
+             <div className='window'>
+                <input id='nTit' onChange={handleInput} type='text' className='field' placeholder='Title...'/>
+                <div className='option' onClick={handleCancel}>Cancel</div>
+                <div className='option' onClick={()=>handleCreate()}>Create</div>
+            </div>
+            <div className="dim"></div>
             <Navbar/>
             <List/>
         </div>
